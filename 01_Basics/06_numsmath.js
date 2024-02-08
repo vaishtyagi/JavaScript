6f3be69faa9abeaef232bc45869aca0c6ec84996
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-08 15:22:59 +0000

    num and math

---
+++ 01_Basics/06_numsmath.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 1234.678
+//console.log(otherNum.toPrecision(5));
+
+const hundreds = 10000000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+/********************************* Math lib********************************************/
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(9.43));
+// console.log(Math.ceil(7.4));
+// console.log(Math.floor(7.4));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min +1)) +1);
