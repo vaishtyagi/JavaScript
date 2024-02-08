643cd8a3667345b81087904a4ad793aa28371642
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-08 13:52:47 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,18 @@
+const Name = "vaish"
+const repoCount = 5
+
+// console.log( Name +  " has " + repoCount + " repos" ) ;
+// console.log(`hello my name is ${ Name} and my repo count is ${repoCount}`);// called string interpolation
+ const bookName = new String('vaishnavi-tyagi-1-heyy')
+
+//  console.log(bookName[7]);
+//  console.log(bookName[9]);
+//  console.log(bookName.length);
+//  console.log(bookName.toUpperCase());
+//  console.log(bookName.charAt(2));
+//  console.log(bookName.indexOf('i'));
+const newString = "     helloooo    "
+console.log(newString);
+console.log(newString.trim());
+
+console.log(bookName.split('-'));
