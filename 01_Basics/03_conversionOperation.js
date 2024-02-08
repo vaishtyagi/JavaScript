048d67daf0795a163860201cd304d65dc0d1e747
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-08 06:54:18 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score= "56vaish"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);// NaN means not a number
+
+let isLoggedIn = ""
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log( booleanisLoggedIn );
+// 1=> true; 0=> false
+//"" => false; "asf"=> true
