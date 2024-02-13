e8c88cf62c4ccdfa1d0a5e4bc38e7a85ced74812
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-13 22:56:10 +0530

    for of loop

---
+++ practice.js
@@ -0,0 +1,13 @@
+//let namee = ["Vaishnavi Tyagi"];
+// for( char of "namee"){
+//      console.log(char);
+// }
+
+
+let heros =[["superman","batman"],["wonderwoman","thor"]];
+
+for(list of heros){
+    for(name of list){
+        console.log(name);
+    }
+}
