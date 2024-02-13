c039ee159b2e195ded2630d2364dac894fcc33b3
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-13 22:56:56 +0530

    functions

---
+++ 1_func.js
@@ -0,0 +1,32 @@
+// function hello(){
+//     console.log("hello");
+
+// }
+
+// hello();
+
+                 //dice rollingg
+
+// function rollDice(){
+//     let rand =Math.floor(Math.random()*6) +1;
+// console.log(rand);
+// }
+
+// rollDice();
+
+                   //function with arguments
+
+// function printInfo (name,age){
+//     console.log(`${name}'s age is ${age}`);
+// }
+// printInfo("Vaishnavi",20);
+
+
+                    //table
+function table(n){
+    for(let i=n;i<=n*10;i+=n){
+        console.log(i);
+    }
+}
+
+table(9);
