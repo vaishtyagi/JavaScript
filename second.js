055be2f694d5073642f0375a2cd9679127b97793
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-13 10:07:08 +0530

    whileNestedArray

---
+++ second.js
@@ -0,0 +1,23 @@
+        //nested loop
+
+// for (let i=1;i<=3;i++){
+//     console.log(`outer loop ${i}`);
+//     for(let j=1;j<=3;j++){
+//         console.log(j);
+//     }
+// }
+
+        //while loop
+//  let i=1;
+//  while(i<=5){
+//     console.log(i);
+//     i++
+//  }   
+       //array loops
+       
+       let fruits = ["apple","mango","banana","papaya","grapes","pineapple"];
+
+       for(let i=0; i< fruits.length ; i++){
+        console.log( i ,fruits[i]);
+    }
+    
