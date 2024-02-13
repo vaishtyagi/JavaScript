072dc94bf844c0bf0c83c8f37ea5ba207ef19198
vaishtyagi <98148270+vaishtyagi@users.noreply.github.com>
2024-02-13 10:06:20 +0530

    forLoops

---
+++ first.js
@@ -0,0 +1,16 @@
+// for (let i=1;i<=5;i++){
+//     console.log(i);
+// }
+// for(let i=5;i>=1;i--){
+//     console.log(i);
+//}
+// for( let i =1;i<=15;i=i+2){
+//     console.log(i);
+//}
+// for(let i=2;i<=10;i=i+2){
+//     console.log(i);
+// }
+let n = prompt("write a no.");
+for(let i=n;i<=n*10;i=i+n){
+    console.log(i);
+}
